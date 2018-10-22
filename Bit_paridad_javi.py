9c010db20772bb24c3d3ac78812fbd27daad7444
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2018-10-22 23:45:19 +0200

    Almost finished 'Bit_paridad_javi.py'

--- Bit_paridad_javi.py
+++ Bit_paridad_javi.py
@@ -15,6 +15,12 @@ class Bit_paridad_javi():
         self.messages_parity = self.add_parity_bit_to_messages()
         self.messages_parity_alterated = []
         self.messages_parity_alterated = self.alterate_messages()
+        self.probabilities = ()
+        self.probabilities = self.calculate_probabilities()
+
+    def get_total_messages(self):
+
+        return len(range(self.start, self.finish +1)) * self.n
 
     def get_messages_raw(self):
 
@@ -27,6 +33,10 @@ class Bit_paridad_javi():
     def get_messages_parity_alterated(self):
 
         return self.messages_parity_alterated
+
+    def get_probabilities(self):
+
+        return self.probabilities
 
     def toString(self):
 
@@ -70,18 +80,39 @@ class Bit_paridad_javi():
 
         return aux_messages_parity_alterated
 
+    def calculate_probabilities(self):
+
+        N = 10**3
+        altered_messages_counter = 0
+        altered_and_detected_messages_counter = 0
+
+        for i in range(len(self.messages_parity_alterated)):
+            received_message_satisfies_parity =  sum(self.messages_parity_alterated[i]) % 2 == 0
+
+            if self.messages_parity[i] != self.messages_parity_alterated[i]:
+                altered_messages_counter += 1
+                if not received_message_satisfies_parity:
+                    altered_and_detected_messages_counter +=1
+            altered_and_no_detected_messages_counter = altered_messages_counter - altered_and_detected_messages_counter
+
+        return altered_messages_counter, altered_and_detected_messages_counter, altered_and_no_detected_messages_counter
 
 
 #Testing
-bitp = Bit_paridad_javi(2,4 ,2, 0.01)
-print("\n\n\n")
+bitp = Bit_paridad_javi(10,1000,50, 0.5)
+print("\n")
 print("Mensajes originales")
 print(bitp.get_messages_raw())
-print("\n\n\n")
+print("\n")
 print("Mensajes con bit de paridad añadido")
 print(bitp.get_messages_parity())
-print("\n\n\n")
+print("\n")
 print("Mensajes alterados")
 print(bitp.get_messages_parity_alterated())
+print("\n")
+print("Mensajes totales generados {}".format(bitp.get_total_messages()))
+print("Mensajes alterados: {}".format(bitp.get_probabilities()[0]))
+print("Mensajes alterados y detectaados {}".format(bitp.get_probabilities()[1]))
+print("Mensajes alterados y  no detectados {}".format(bitp.get_probabilities()[2]))
 
 
