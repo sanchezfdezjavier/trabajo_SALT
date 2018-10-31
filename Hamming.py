c3e92f8f92d3f89f897bbb8c8f2e63d00c474639
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2018-10-31 17:39:21 +0100

    Hamming aparentemente acabado

--- Hamming.py
+++ Hamming.py
@@ -7,12 +7,12 @@ from collections import namedtuple
 
 
 
-def generar_mensaje(k): 
+def generar_mensaje(k):
     # Dev. un mensaje de datos (0s y 1s) de la longitud máxima que se puede
-    # autocorregir con k bits de paridad:  
+    # autocorregir con k bits de paridad:
     # Ejemplo: si k = 3, long = 4 (7, incluyendo los bits de paridad, 8
     # incluyendo la situación de no error)
-                                                              
+
     longitud_mensaje = 2**k - 1 - k
     mensaje = []
     for _ in range(longitud_mensaje):
@@ -33,23 +33,23 @@ def cifra_binaria(n, k):
     for _ in range(k):
         n = n // 2
     return n % 2
-    
+
 def insertar_bits_de_paridad(mensaje, k):
     # Insertamos 0s en las posiciones (2**0, 2**1, ...) de los bits de paridad:
     for i in range(k):
         mensaje.insert(2**i - 1, 0)
-        
-    # Calculamos el valor real de los k bits de paridad: 
-    for i in range(k): 
+
+    # Calculamos el valor real de los k bits de paridad:
+    for i in range(k):
         # Calculamos el bit de paridad i-ésimo:
         suma_de_bits = 0
         for j in range(len(mensaje)):
-            suma_de_bits += cifra_binaria(j+1, i) * mensaje[j] 
+            suma_de_bits += cifra_binaria(j+1, i) * mensaje[j]
         mensaje[2**i-1] = suma_de_bits % 2
- 
+
 def alterar_lista_de_bits(lista, p):
     for i in range(len(lista)):
-        alterar = random.random() < p 
+        alterar = random.random() < p
         if alterar:
             lista[i] = 1 - lista[i]
 
@@ -94,14 +94,14 @@ def Hamming(k, p, iter):
 
         bits_alterados = bits_de_paridad_alterados(mensaje_emitido, k)
         posicion_error = posicion_del_error(bits_alterados)
-        
+
         if mensaje_emitido != mensaje_original:
             contador_mensajes_alterados += 1
             # Corregir el mensaje:
             corregir_bit_erroneo(mensaje_emitido, posicion_error)
             if mensaje_emitido == mensaje_original:
                 contador_mensajes_alterados_y_corregidos_bien += 1
-    contador_mensajes_alterados_y_corregidos_mal = contador_mensajes_alterados - contador_mensajes_alterados_y_corregidos_bien     
+    contador_mensajes_alterados_y_corregidos_mal = contador_mensajes_alterados - contador_mensajes_alterados_y_corregidos_bien
     return contador_mensajes_alterados/iter, contador_mensajes_alterados_y_corregidos_bien/iter, contador_mensajes_alterados_y_corregidos_mal/iter
 
 def simulacion(iter):
@@ -121,6 +121,7 @@ def get_parameters_plot():
     list_p_alterado  = []
     list_p_alteado_y_corregido = []
     list_p_alterado_y_no_corregido = []
+    indices = []
 
     for k in [3, 4, 5, 6, 7, 8, 9]:
         n = 2**k - 1 - k
@@ -130,55 +131,57 @@ def get_parameters_plot():
             list_p_alterado.append(p_alterado)
             list_p_alteado_y_corregido.append(p_alterado_y_corregido)
             list_p_alterado_y_no_corregido.append(p_alterado_y_corregido)
+            indices.append(repr(n).rjust(4) + " + " + repr(k))
 
 
-    return list_p_alterado, list_p_alteado_y_corregido, list_p_alterado_y_no_corregido
+    return indices, list_p_alterado, list_p_alteado_y_corregido, list_p_alterado_y_no_corregido
 
 def test_barchart():
-    n_groups = 5
 
-    means_men = (20, 35, 30, 35, 27)
-    std_men = (2, 3, 4, 1, 2)
+    n_groups = len(get_parameters_plot()[1])
 
-    means_women = (25, 32, 34, 20, 25)
-    std_women = (3, 5, 2, 3, 3)
+    means_men = get_parameters_plot()[1]
 
-    other_values = (35, 20, 25, 30, 27)
-    std_other = (5, 4, 2, 4, 3)
+
+    means_women = get_parameters_plot()[2]
+
+
+    other_values = get_parameters_plot()[3]
+
 
     fig, ax = plt.subplots()
 
     index = np.arange(n_groups)
-    bar_width = 0.15
+    bar_width = 0.25
 
-    opacity = 0.4
+    opacity = 0.8
     error_config = {'ecolor': '0.3'}
 
     rects1 = ax.bar(index, means_men, bar_width,
                     alpha=opacity, color='b',
-                    yerr=std_men, error_kw=error_config,
-                    label='Men')
+                    error_kw=error_config,
+                    label='alterado')
 
     rects2 = ax.bar(index + bar_width, means_women, bar_width,
                     alpha=opacity, color='r',
-                    yerr=std_women, error_kw=error_config,
-                    label='Women')
+                    error_kw=error_config,
+                    label='alterado y corregido')
     rects3 = ax.bar(index + bar_width + bar_width, other_values, bar_width,
                     alpha=opacity, color = 'g',
-                    yerr=std_other, error_kw=error_config,
-                    label= 'Other')
+                    error_kw=error_config,
+                    label= 'alterado y no corregido')
 
-    ax.set_xlabel('Group')
-    ax.set_ylabel('Scores')
-    ax.set_title('Scores by group and gender')
+    ax.set_xlabel('messages length')
+    ax.set_ylabel('messages number')
+    ax.set_title('Hamming results')
     ax.set_xticks(index + bar_width / 2)
-    ax.set_xticklabels(('A', 'B', 'C', 'D', 'E'))
+    ax.set_xticklabels(get_parameters_plot()[0], fontsize = 3.5)
     ax.legend()
 
     fig.tight_layout()
     plt.show()
 
 
-simulacion(10)
+simulacion(1)
 test_barchart()
 print(get_parameters_plot())
